390055eca2c67d5738262a553caf67e93638f683
Juriri <lhj7826@naver.com>
2023-07-31 15:17:02 +0900

    20230731 15:16 달력 넘기기 성공 업데이트

---
+++ Test/src/main/java/calender/calController.java
@@ -0,0 +1,116 @@
+package calender;
+
+import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class calController
+ */
+@WebServlet("/calender/Cal")
+public class calController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public calController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String action = request.getParameter("action");
+		LocalDate currentDate = LocalDate.parse(request.getParameter("currentDate"));
+
+		int year = currentDate.getYear();
+        int month = currentDate.getMonthValue();
+        int date = currentDate.getDayOfMonth();
+        
+        String dayOfWeekStr = currentDate.getDayOfWeek().name();
+        
+        LocalDate firstDayOfMonth = currentDate.withDayOfMonth(1);
+        LocalDate lastDayOfMonth = currentDate.withDayOfMonth(currentDate.lengthOfMonth()); 
+        
+        
+        
+		if (action.equals("previous")) {
+	        // 이전 달 계산
+	        if (month == 1) {
+	        	year = year - 1;
+	        	month = 12;
+	        } else {
+	        	month = month - 1;
+	        }	
+	        
+		} else if (action.equals("next")) {     
+	        // 다음 달 계산
+	        if (month == 12) {
+	        	year = year + 1;
+	        	month = 1;
+	        } else {
+	        	month = month + 1;
+	        }
+		}
+	
+
+		//가장 마지막 date 28 or 30 or 31 계산
+        if (month == 2) {
+            // February has a special case, so handle it separately
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                // Leap year
+                date = 29;
+            } else {
+                date = 28;
+            }
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            // 30-day month
+            date = Math.min(date, 30);
+        } else {
+            // 31-day month
+            date = Math.min(date, 31);
+        }
+        
+		//전달 혹은 다음달 날짜 전달
+		HttpSession session = request.getSession();
+		String new_month = "";
+		if (0<month && month< 10)
+			new_month = "0"+Integer.toString(month);
+		else
+			new_month = Integer.toString(month);
+		
+		//str -> LocalDate로 변경
+		String new_str = year+"-"+new_month+"-"+date;
+		LocalDate nextDate = LocalDate.parse(new_str);
+		
+		session.setAttribute("nextDate", nextDate);
+		
+		String path = "/calender/moveMonth.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+		if (dispatcher != null) {
+			dispatcher.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- Test/src/main/webapp/calender/calender.jsp
+++ Test/src/main/webapp/calender/calender.jsp
@@ -1,12 +1,60 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"
-    pageEncoding="EUC-KR"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" import="java.time.*" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="EUC-KR">
-<title>Insert title here</title>
+<meta charset="UTF-8">
+<title>일정 관리</title>
+<link rel="stylesheet" href="${pageContext.request.contextPath}/css/board_style.css">
+	<script type="text/javascript">
+	</script>
 </head>
 <body>
+    <% 
+        LocalDate currentDate = LocalDate.now();
+        int year = currentDate.getYear();
+        int month = currentDate.getMonthValue();
+        DayOfWeek dayOfWeek = currentDate.getDayOfWeek();
+        String dayOfWeekStr = dayOfWeek.name();
+    
+     	// 현재 월의 첫날과 마지막 날을 구합니다.
+        LocalDate firstDayOfMonth = currentDate.withDayOfMonth(1);
+        LocalDate lastDayOfMonth = currentDate.withDayOfMonth(currentDate.lengthOfMonth()); 
+     %>
+    <h1><%=year %>년 <%=month %>월</h1>
+    <a href="${pageContext.request.contextPath }/calender/Cal?action=previous&currentDate=<%= currentDate %>">이전 달</a>
+  	<a href="${pageContext.request.contextPath }/calender/Cal?action=next&currentDate=<%= currentDate %>">다음 달</a>
+    <table border="1">
+        <tr>
+            <th>일</th>
+            <th>월</th>
+            <th>화</th>
+            <th>수</th>
+            <th>목</th>
+            <th>금</th>
+            <th>토</th>
+        </tr>
+        <tr>
+            <%-- 첫 주의 공백 셀 만들기 --%>
+            <% for (int i = 1; i < firstDayOfMonth.getDayOfWeek().getValue(); i++) { %>
+                <td></td>
+            <% } %>
 
+            <%-- 해당 월의 날짜 출력 --%>
+            <% for (LocalDate date = firstDayOfMonth; date.isBefore(lastDayOfMonth.plusDays(1)); date = date.plusDays(1)) { %>
+                <td><%= date.getDayOfMonth() %></td>
+
+                <%-- 토요일이면 줄 바꾸기 --%>
+                <% if (date.getDayOfWeek() == DayOfWeek.SATURDAY) { %>
+                    </tr><tr>
+                <% } %>
+            <% } %>
+
+            <%-- 마지막 주의 공백 셀 만들기 --%>
+            <% for (int i = lastDayOfMonth.getDayOfWeek().getValue(); i < 7; i++) { %>
+                <td></td>
+            <% } %>
+        </tr>
+    </table>
 </body>
 </html>
